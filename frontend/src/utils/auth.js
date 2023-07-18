c00e8def41cc51ce6cc9192a51c31cb1b9c3568e
Dmitriy Saprykin <saprykindima@bk.ru>
2023-07-19 01:44:34 +0300

    initial commit

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -28,6 +28,7 @@ class Auth {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: 'include',
       body: JSON.stringify({ email, password }),
     }).then(this._getResponseData);
   }
