8966ed1b0b32bd60d843aeec9b5b2a731222a6ba
Dmitriy Saprykin <saprykindima@bk.ru>
2023-07-19 00:33:21 +0300

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const cors = require('./middlewares/cors');
+//const cors = require('./middlewares/cors');
+const cors = require('cors');
 const userRoutes = require('./routes/userRouter');
 const cardRoutes = require('./routes/cardRouter');
 const { login, createUser } = require('./controllers/users');
@@ -15,7 +16,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 4000 } = process.env;
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mestodb')
   .then(() => {
